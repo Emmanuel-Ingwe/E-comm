075a9f727a5f30b2241ea8876f922b4be17bf1bf
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-07-14 16:02:08 +0100

    Promise return

---
+++ app.js
@@ -0,0 +1,10 @@
+function changeColor(color, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.background = color;
+            resolve();
+        }, delay);
+    });
+}
+
+changeColor('teal', 1000);
