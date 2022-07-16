def9a280b1d1632d9c88e39b96fed7f64f29c69e
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-07-16 23:43:22 +0100

    Footer & Md Queries||

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-function changeColor(color, delay) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            document.body.style.background = color;
-            resolve();
-        }, delay);   
-    });
-}
+// function changeColor(color, delay) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             document.body.style.background = color;
+//             resolve();
+//         }, delay);   
+//     });
+// }
 
-changeColor('teal', 1000);
+// changeColor('teal', 1000);
+
+const menuItems = document.querySelector('.menu-items');
+
+menuItems.style.maxHeight = '0px';
+
+function menutoggle() {
+    if (menuItems.style.maxHeight === "0px") {
+        menuItems.style.maxHeight = "200px";
+    } else {
+        menuItems.style.maxHeight = '0px';
+    }
+}
